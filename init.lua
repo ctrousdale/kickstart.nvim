eb1bb383c005415c3101c968a29ae91cd725da1a
Chandler Trousdale <chandler2506@gmail.com>
2025-05-16 00:41:53 -0400

    Add startup-validation
    Will add more checks in the future.

--- init.lua
+++ init.lua
@@ -93,6 +93,8 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+require('startup-validation').validate_nvim_install()
+
 require 'options'
 
 require 'keymaps'
